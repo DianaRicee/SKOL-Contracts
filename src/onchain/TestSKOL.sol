f2caf30611ecb6b7563203a1ee97b12fdfcafe2a
DianaRicee <DianaRicee@outlook.com>
2025-10-12 19:03:11 -0700

    feat: Create TestSKOL Contract Natspec Comments

--- src/onchain/TestSKOL.sol
+++ src/onchain/TestSKOL.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title TestSKOL
+ * @dev Simplified version of ReputationRegistry for testnet testing
+ * @notice Removes restrictions to allow easy testing with multiple wallets
+ */
 contract TestSKOL {}
