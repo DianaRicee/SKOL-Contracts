6d58181fbb641d08a9ea66e64b58e5f55de84292
DianaRicee <DianaRicee@outlook.com>
2025-10-22 12:02:43 -0700

    feat: Add isRegistered External View Function Natspec Comments

--- src/onchain/TestSKOL.sol
+++ src/onchain/TestSKOL.sol
@@ -149,6 +149,11 @@ contract TestSKOL {
         return _reputations[user];
     }
 
+    /**
+     * @dev Check if user is registered
+     * @param user Address of the user
+     * @return Whether user is registered
+     */
     function isRegistered(address user) external view returns (bool) {
         return _reputations[user].isRegistered;
     }
