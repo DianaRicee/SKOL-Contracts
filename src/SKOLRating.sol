96707af9b8f3d7062a2fef923b61ceb3dbf407f6
DianaRicee <DianaRicee@outlook.com>
2025-10-28 11:22:45 -0700

    feat: Add ISKOLRegistry Interface w/ Functions

--- src/SKOLRating.sol
+++ src/SKOLRating.sol
@@ -5,9 +5,25 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 
+interface ISKOLRegistry {
+    function getReputation(address user) external view returns (uint256);
+    function updateReputation(address user, uint256 rating, address rater) external;
+    function getReputationData(address user)
+        external
+        view
+        returns (
+            uint256 score,
+            uint256 totalRatings,
+            uint256 totalScore,
+            uint256 lastUpdateTime,
+            uint256 lastDecayTime,
+            bool isRegistered
+        );
+}
+
 /**
  * @title SKOLRating
  * @dev Handles rating submissions between users with anti-gaming mechanisms
  * @notice This contract manages the rating process and enforces business rules
  */
-contract SKOLRating  {}
+contract SKOLRating {}
